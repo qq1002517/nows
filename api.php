102fb4966225f4017bc964a9ac7f62d130f68ac6
haobo56 <1002517@163.com>
2023-12-07 11:27:47 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,10 +1,16 @@
 <?php
+  error_reporting(0);
   //设置json格式头部
   header('Content-type: application/json');
   //载入数据库文件
   require("data.php");
   $sql="select * from soul order by rand( ) limit 1";
-  $row=mysql_fetch_assoc(mysql_query($sql));
-  //输出json
-  echo json_encode(array('code'=>1,'data'=>$row['title']));
-?>		
+  $result=mysqli_query($conn,$sql);
+  if (mysqli_num_rows($result) > 0) { 
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo json_encode(array('code'=>1,'data'=>$row['title']));
+        }    
+    } else {
+        echo "0 results";
+    }
+?> 
