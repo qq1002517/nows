31cc4a005de359e2f16727388df4fad5d800b6bc
haobo56 <1002517@163.com>
2023-12-07 11:28:26 +0800

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,5 @@
 <?php
-	$conn=mysql_connect("localhost","symhytwg_fun","wahaha2019"); //连接数据库地址、用户名、密码
-	mysql_query("set names 'utf8'"); //数据库编码
-	mysql_select_db("sy_nows"); //数据库名称
+        $conn=mysqli_connect("127.0.0.1","root","demo@pwds","demo1",13306); //连接数据库地址、用户名、密码
+        #mysql_query("set names 'utf8'"); //数据库编码
+        #mysql_select_db("demo1"); //数据库名称
 ?>
